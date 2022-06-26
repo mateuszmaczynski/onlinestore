8c8a5f22015e9285aeac0c20190742bae90f4bc3
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2022-06-26 21:04:02 +0200

    add document.tsx to update html tags used to render a page

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="pl">
+      <Head />
+      <body className="bg-body text-body font-body">
+      <Main />
+      <NextScript />
+      </body>
+    </Html>
+  )
+}
