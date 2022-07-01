2f2288fe3a4db0416d71e29866abe84f95023a85
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2022-07-01 22:54:31 +0200

    modify page made in CSR way

--- pages/products-csr.tsx
+++ pages/products-csr.tsx
@@ -1,8 +1,10 @@
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Main } from "../components/Main";
+import { ProductListItem } from "../components/Product";
 import { useQuery } from "react-query";
 
+// CSR
 export interface ProductProps {
   category: string;
   description: string;
@@ -17,14 +19,13 @@ export interface ProductProps {
 }
 
 const getProducts = async () => {
-  const res = await fetch(`http://fakestoreapi.com/products/`);
+  const res = await fetch('https://naszsklep-api.vercel.app/api/products');
   const data: ProductProps[] = await res.json();
   return data;
 };
 
 const ProductPageCSR = () => {
   const {data, isLoading, error} = useQuery("products", getProducts);
-
   if (isLoading){
     return <div>Loading...</div>
   }
@@ -40,9 +41,15 @@ const ProductPageCSR = () => {
           <div className="p-16">
             <ul className="w-full grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
               {data.map((product) => (
-                 <div>
-                   {product.title}
-                 </div>
+                 <ProductListItem data={{
+                   id: product.id,
+                   price: product.price,
+                   rating: product.rating.rate,
+                   thumbnailUrl: product.image,
+                   thumbnailAlt: product.title,
+                   title: product.title
+                 }}
+                 />
               ))}
             </ul>
           </div>
