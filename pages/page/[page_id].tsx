16bde19d279ee1cee57bd612e1896dbe3c1320c1
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2022-07-11 21:26:47 +0200

    add generic pageId

---
+++ pages/page/[page_id].tsx
@@ -0,0 +1,16 @@
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { Main } from "../../components/Main";
+
+const PageId = () => {
+    return (
+        <div className=" flex flex-col min-h-screen ">
+            <Header />
+            <Main>
+            </Main>
+            <Footer />
+        </div>
+    )
+}
+
+export default PageId;
