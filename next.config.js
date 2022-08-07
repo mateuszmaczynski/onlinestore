bea04ea64f4e37c14b7354fdc6003639663d501d
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2022-08-07 23:10:24 +0200

    add apolloClient

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["naszsklep-api.vercel.app"],
+    domains: [
+      "naszsklep-api.vercel.app",
+      "media.graphassets.com"
+    ],
     formats: [
       'image/avif', 'image/webp'
     ]
