ee345781f1948783f244255ebe9c9e87703bbc38
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2022-08-19 22:46:40 +0200

    create page for checkout

---
+++ pages/checkout.tsx
@@ -0,0 +1,14 @@
+import { Main } from "../components/Main";
+import CheckoutForm from "../components/CheckoutForm";
+
+const CheckoutPage = () => {
+  return (
+    <div className=" flex flex-col min-h-screen ">
+      <Main>
+        <CheckoutForm />
+      </Main>
+    </div>
+  );
+};
+
+export default CheckoutPage;
