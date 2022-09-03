33ad8b31875cc4a4640e63fa006e19b267df6fdc
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2022-09-03 18:32:44 +0200

    changing test endpoint

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type {NextApiHandler, NextApiRequest, NextApiResponse} from "next";
 
 type Data = {
   name: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: "John Doe" });
-}
+const handler: NextApiHandler = (req, res) => {
+  if(req.method !== "POST"){
+    return res.setHeader('ALLOW',"POST").status(405).json({});
+  }
+  res.status(200).json({ body: req.body });
+
+
+};
+
+export default handler;
+
+
