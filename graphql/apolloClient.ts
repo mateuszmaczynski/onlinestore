934f56132cc6c3140a37fe81683686261e60bf97
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2022-09-10 18:40:35 +0200

    minor changes

--- graphql/apolloClient.ts
+++ graphql/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  // should be this uri in .env
+  // should be this uri in .env.local
   uri: "https://api-eu-central-1.hygraph.com/v2/cl6js8w1u5f2s01undyf2h5oz/master",
   cache: new InMemoryCache(),
 });
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ["naszsklep-api.vercel.app", "media.graphassets.com"],
     formats: ["image/avif", "image/webp"],
